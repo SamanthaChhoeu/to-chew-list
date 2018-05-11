9a098bc22f47f2e47aa9bc9bf86245e1acdadc52
jacksehr <jackshe2702@gmail.com>
2018-05-11 22:34:52 +1000

    Decoding bugfix

--- news/spell.py
+++ news/spell.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 import http.client, urllib.parse, json
 
 def translate(news_type):
@@ -17,10 +19,13 @@ def translate(news_type):
     body = urllib.parse.urlencode (data)
     conn.request ("POST", path + params, body, headers)
     response = conn.getresponse ()
-    output = json.loads(response.read())['flaggedTokens']
+    output = json.loads(response.read().decode())['flaggedTokens']
     output = [suggestion for suggestion in output if suggestion['token'] == news_type]
-    suggestions = output[0]['suggestions']
-    return sorted(suggestions, key=lambda x: x['score'])[0]['suggestion']
+    if output:
+        suggestions = output[0]['suggestions']
+        return sorted(suggestions, key=lambda x: x['score'])[0]['suggestion']
+    else:
+        return ""
 
 if __name__ == '__main__':
     word = input("Select type of news: ")
