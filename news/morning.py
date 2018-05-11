700eb9398f28f56b401bd3b1799ae2a2f426c11c
GeoKinetic <gwmountakis@gmail.com>
2018-05-12 00:10:11 +1000

    morning functionality finished up to send to bot

--- news/morning.py
+++ news/morning.py
@@ -1,26 +1,16 @@
-import news_req
+from news_req import news_req
 
-def core ():
-    #article_dict = news_req()
-    articles = news_req()
+def parse_news ():
+    articles = news_req("au")
     articles = articles[:3]
 
-    """
-    output = {}
-    output["articles"] = []
-
-    for i in range(3):
-        temp = {}
-        temp["title"] = articles[i]["title"]
-        temp["description"] = articles[i]["description"]
-        temp["url"] = 
-        output["articles"][i] = articles[i]
-    """
-
-    keep = {'title', 'description', 'url'}
+    keep = {'title', 'url'}
     for index, article in enumerate(articles):
         for key in set(article.keys()) - keep:
             del articles[index][key]
 
     print(articles)
+
     #send to fb api
+
+parse_news()
