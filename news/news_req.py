b19bb454f4d5cd10ffdf905fdc0fc987ed6fe966
jacksehr <jackshe2702@gmail.com>
2018-05-12 00:59:56 +1000

    Added spellcheck to news_req

--- news/news_req.py
+++ news/news_req.py
@@ -1,17 +1,33 @@
 from newsapi import NewsApiClient
+from spell import translate
 
 KEY = "5c382368f74c48b18bf495cd990e1bd3"
+categories = ['business', 'entertainment', 'general', 'health', 
+              'science', 'sports', 'technology']
 
-def news_req(region, query=None): 
+def news_req(region='au', query=None): 
     # Initiate client 
     client = NewsApiClient(api_key=KEY)
 
     # Get headlines
     if not query:
-        headlines = client.get_top_headlines(country=region)
+        headlines = client.get_top_headlines(country=region, sources='google-news')
     else:
-        headlines = client.get_everything(q=query, country=region)
+        corrected = translate(query)
+        final_category = None
+        for category in categories:
+            if category.startswith(query) or category.startswith(corrected):
+                final_category = category
+                break
+        if not final_category:
+            headlines = client.get_everything(
+                q=query, 
+                sort_by='relevancy',
+                sources='google-news'
+            )
+        else:
+            headlines = client.get_top_headlines(
+                category=final_category, 
+                country=region, 
+            )
     return headlines.get('articles')
-
-if __name__ == '__main__':
-    print(news_req('au'))
